ebfef0985b86b922e7b6ac169d70ff7164e7aee9
Coffeecats900 <136274009+Coffeecats900@users.noreply.github.com>
2025-05-13 11:26:33 -0400

    reset the files

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-poetry install --no-root || ( pause & exit /b )
-poetry run python main.py || ( pause & exit /b )
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit on failure
-
-# cd into script location
-cd "$(dirname "$0")"
-poetry install --no-root
-poetry run python main.py
